dd702bb5eda587dc5daf465bc2d3ccb60ba3718e
ResourceCoin <admin@resourcecoin.io>
2017-12-28 09:49:48 +0400

    1.0.0

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define ResourceCoin-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "ResourceCoinURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
